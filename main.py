3c053a6c15fb95d64f719aa3278daa3e0bbd71f1
iheb <iheb66@yahoo.com>
2021-01-14 21:53:29 +0100

    tree visualized

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from tree import Tree
 from anytree import RenderTree
+from anytree.exporter import DotExporter
 
 
 
 def Game():
     number_of_sticks=int(input("enter number of sticks:"))
-    tree=Tree(number_of_sticks,True)
-    print(RenderTree(tree.RootNode).by_attr(lambda n: ("-".join(map(str, n.node_value)) +
-                                                "  [" + str(n.evaluator_value) + "]")))
+    tree = Tree(number_of_sticks,True)
+    # print(RenderTree(tree.RootNode).by_attr(lambda n: ("-".join(map(str, n.node_value)) +
+    #                                             "  [" + str(n.evaluator_value) + "]")))
+    DotExporter(tree.RootNode,nodenamefunc=lambda node: node.node_value).to_picture("test.png")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- tree.py
+++ tree.py
@@ -6,6 +6,7 @@ class Tree:
         self.RootNode = Node(str(rootValue),node_value=[rootValue], is_root=True,evaluator_value=None)
         self.isFirst = isFirst
         self.generateTree(self.RootNode)
+        self.evaluateTreeMinMax(self.RootNode)
     def generateTree(self,currentNode):
         maxValue=max(currentNode.node_value)
         if maxValue <= 2:
@@ -13,10 +14,38 @@ class Tree:
         else:
             currentNodeValues=currentNode.node_value.copy()
             for index,value in enumerate(currentNodeValues):
-                totalChildren=(int(value/2)-1)if value%2==0 else int(value/2)
+                totalChildren=(int(value/2)-1)if value % 2 == 0 else int(value/2)
                 for i in range(1,totalChildren+1):
                     childValue = currentNodeValues.copy()
                     childValue[index] -= i
                     childValue.insert(index+1,i)
                     child = Node(currentNode.name+"-"+str(i),parent=currentNode,node_value=childValue,evaluator_value=None)
                     self.generateTree(child)
+    def evaluateTreeMinMax(self,currentNode:Node):
+        if currentNode.is_leaf:
+            currentNode.evaluator_value = 1 if (currentNode.depth % 2 == 1) else -1
+        else:
+            childEvaluateValues = []
+            for child in currentNode.children:
+                self.evaluateTreeMinMax(child)
+                childEvaluateValues.append(child.evaluator_value)
+            evaluate = max(childEvaluateValues) if (currentNode.depth%2 == 0)else min(childEvaluateValues)
+            currentNode.evaluator_value=evaluate
+    def evaluateTreeAlphaBeta(self,currentNode:Node,parentEvaluator:int=None):
+        if currentNode.is_leaf:
+            currentNode.evaluator_value = 1 if (currentNode.depth % 2 == 1)else -1
+        else:
+            childEvaluateValues = []
+            current_evaluation: int = None
+            is_current_max = currentNode.depth % 2 == 0
+            for child in currentNode.children:
+                self.evaluateTreeMinMax(child,current_evaluation)
+                if child.evaluator_value:
+                    if parentEvaluator:
+                        skipped: bool = (child.evaluator_value >= parentEvaluator)if is_current_max else (
+                                    parentEvaluator <= child.evaluator_value)
+                        if skipped:
+                            return
+                    if not(current_evaluation) or (is_current_max == child.evaluator_value>current_evaluation):current_evaluation=child.evaluator_value
+                    childEvaluateValues.append(child.evaluator_value)
+            evaluate = max(childEvaluateValues) if is_current_max else min(childEvaluateValues)
