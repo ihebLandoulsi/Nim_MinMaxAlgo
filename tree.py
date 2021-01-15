a506adffc847285800b007b5fd42b79ed99ebcda
iheb <iheb66@yahoo.com>
2021-01-15 01:26:10 +0100

    Min Max Algorithm implemented

--- tree.py
+++ tree.py
@@ -1,19 +1,23 @@
 from anytree import Node
-
+from anytree import RenderTree
 
 class Tree:
-    def __init__(self, rootValue:int, isFirst:bool):
-        self.RootNode = Node(str(rootValue),node_value=[rootValue], is_root=True,evaluator_value=None)
+    def __init__(self, rootValue:int, isFirst:bool,isAlgoMinMax:bool):
+        self.rootNode = Node(str(rootValue), node_value=[rootValue], is_root=True, evaluator_value=None)
         self.isFirst = isFirst
-        self.generateTree(self.RootNode)
-        self.evaluateTreeMinMax(self.RootNode)
+        self.nodesVisited: int = 0
+        self.generateTree(self.rootNode)
+        if isAlgoMinMax:
+            self.evaluateTreeMinMax(self.rootNode)
+        else:
+            self.evaluateTreeAlphaBeta(self.rootNode, None)
     def generateTree(self,currentNode):
         maxValue=max(currentNode.node_value)
         if maxValue <= 2:
             return
         else:
             currentNodeValues=currentNode.node_value.copy()
-            for index,value in enumerate(currentNodeValues):
+            for index, value in enumerate(currentNodeValues):
                 totalChildren=(int(value/2)-1)if value % 2 == 0 else int(value/2)
                 for i in range(1,totalChildren+1):
                     childValue = currentNodeValues.copy()
@@ -31,6 +35,7 @@ class Tree:
                 childEvaluateValues.append(child.evaluator_value)
             evaluate = max(childEvaluateValues) if (currentNode.depth%2 == 0)else min(childEvaluateValues)
             currentNode.evaluator_value=evaluate
+        self.nodesVisited += 1
     def evaluateTreeAlphaBeta(self,currentNode:Node,parentEvaluator:int=None):
         if currentNode.is_leaf:
             currentNode.evaluator_value = 1 if (currentNode.depth % 2 == 1)else -1
@@ -39,7 +44,7 @@ class Tree:
             current_evaluation: int = None
             is_current_max = currentNode.depth % 2 == 0
             for child in currentNode.children:
-                self.evaluateTreeMinMax(child,current_evaluation)
+                self.evaluateTreeAlphaBeta(child,current_evaluation)
                 if child.evaluator_value:
                     if parentEvaluator:
                         skipped: bool = (child.evaluator_value >= parentEvaluator)if is_current_max else (
@@ -49,3 +54,8 @@ class Tree:
                     if not(current_evaluation) or (is_current_max == child.evaluator_value>current_evaluation):current_evaluation=child.evaluator_value
                     childEvaluateValues.append(child.evaluator_value)
             evaluate = max(childEvaluateValues) if is_current_max else min(childEvaluateValues)
+            currentNode.evaluator_value=evaluate
+        self.nodesVisited += 1
+    def __str__(self)->str:
+        return RenderTree(self.rootNode).by_attr(lambda n: ("-".join(map(str, n.node_value)) +
+                                                "  [" + str(n.evaluator_value) + "]"))
